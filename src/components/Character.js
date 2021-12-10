2d53b8b23c16a6e0da46890bc7a89cde3c459772
Andrew Nelson <Realseal177@gmail.com>
2021-12-10 10:40:44 -0800

    tests passed

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+import React from "react";
+import App from '../App';
+
+
+export default function StarWars({info}) {
+  //const { info } = props;
+  //console.log(props);
+
+  return (
+    <div className="charList">
+      <h2 className="name">
+        {info.name}
+      </h2>
+    </div>
+  );
+}
